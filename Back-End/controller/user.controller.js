4ee569d2ce8c89169433f0f0853a0306ce2acc09
Caroline Victor-Pujebet <caroline_pujebet@hotmail.com>
2021-02-12 15:12:34 +0000

    style: clean code

--- Back-End/controller/user.controller.js
+++ Back-End/controller/user.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const Model = require('../models/users.model');
+const User = require('../models/users.model');
 
 exports.getUserById = async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await Model.findById(id);
+    const user = await User.findById(id);
     res.send(user);
   } catch (err) {
     console.log(err);
@@ -16,7 +16,7 @@ exports.getUserById = async (req, res) => {
 exports.getListByUser = async (req, res) => {
   const { id, listName } = req.params;
   try {
-    const user = await Model.findOne({ _id: id }, listName);
+    const user = await User.findOne({ _id: id }, listName);
     res.send(user[listName]);
   } catch (err) {
     console.log(err);
@@ -25,11 +25,9 @@ exports.getListByUser = async (req, res) => {
 };
 
 exports.addMovieToList = async (req, res) => {
-  // const { id, listName } = req.params;
   const { id, listName, movieToAdd } = req.body;
   try {
-    // let user = await Model.findById(id);
-    let user = await Model.findOneAndUpdate(
+    let user = await User.findOneAndUpdate(
       { _id: id, [listName]: { $nin: [movieToAdd] } },
       { $push: { [listName]: [movieToAdd] } },
       { new: true },
@@ -49,28 +47,28 @@ exports.addMovieToList = async (req, res) => {
 exports.addMovieToDislikedList = async (req, res) => {
   const { id, movieToAdd } = req.body;
   try {
-    let user = await Model.findOneAndUpdate(
+    let user = await User.findOneAndUpdate(
       { _id: id, disliked: { $nin: [movieToAdd] } },
       { $push: { disliked: [movieToAdd] } },
       { new: true },
     );
     if (user !== null) {
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { liked: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { watchlist: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { favourites: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { last_seen: [movieToAdd] },
         { new: true },
@@ -91,28 +89,28 @@ exports.addMovieToDislikedList = async (req, res) => {
 exports.addMovieToLikedList = async (req, res) => {
   const { id, movieToAdd } = req.body;
   try {
-    let user = await Model.findOneAndUpdate(
+    let user = await User.findOneAndUpdate(
       { _id: id, liked: { $nin: [movieToAdd] } },
       { $push: { liked: [movieToAdd] } },
       { new: true },
     );
     if (user !== null) {
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { disliked: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { watchlist: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { favourites: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { last_seen: [movieToAdd] },
         { new: true },
@@ -133,28 +131,28 @@ exports.addMovieToLikedList = async (req, res) => {
 exports.addMovieToFavourites = async (req, res) => {
   const { id, movieToAdd } = req.body;
   try {
-    let user = await Model.findOneAndUpdate(
+    let user = await User.findOneAndUpdate(
       { _id: id, favourites: { $nin: [movieToAdd] } },
       { $push: { favourites: [movieToAdd] } },
       { new: true },
     );
     if (user !== null) {
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { watchlist: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { disliked: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { liked: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { last_seen: [movieToAdd] },
         { new: true },
@@ -175,23 +173,23 @@ exports.addMovieToFavourites = async (req, res) => {
 exports.addMovieToWatchlist = async (req, res) => {
   const { id, movieToAdd } = req.body;
   try {
-    let user = await Model.findOneAndUpdate(
+    let user = await User.findOneAndUpdate(
       { _id: id, watchlist: { $nin: [movieToAdd] } },
       { $push: { watchlist: [movieToAdd] } },
       { new: true },
     );
     if (user !== null) {
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { liked: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { disliked: { $in: [movieToAdd] } } },
         { new: true },
       );
-      user = await Model.findOneAndUpdate(
+      user = await User.findOneAndUpdate(
         { _id: id },
         { $pull: { favourites: { $in: [movieToAdd] } } },
         { new: true },
--- Back-End/router.js
+++ Back-End/router.js
@@ -3,7 +3,6 @@ const users = require('./controller/user.controller');
 
 router.get('/users/:id', users.getUserById);
 router.get('/lists/:id/:listName', users.getListByUser);
-// router.put('/lists', users.addMovieToList);
 router.put('/lists/disliked', users.addMovieToDislikedList);
 router.put('/lists/liked', users.addMovieToLikedList);
 router.put('/lists/favourites', users.addMovieToFavourites);
